a1090999b72764bd050f411184fa3ae1b93e1c66
mridle582 <mridle582@gmail.com>
2022-07-20 11:21:18 -0400

    added a bmi calculator

--- calculator.js
+++ calculator.js
@@ -4,23 +4,35 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 const port = 3000;
 
 
-app.get('/', function(req, res){
-    // res.send('Hello World');
+// Normal Calc
+app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res){
-    console.log(req.body);
     var n1 = Number(req.body.n1);
     var n2 = Number(req.body.n2);
     var result = n1 + n2;
 
     res.send("The result of the calculation is: " + result);
 });
+
+
+// BMI Calc
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmicalculator", function(req, res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = weight/(Math.pow(height, 2));
+
+    res.send("Caluclated BMI: " + bmi);
+})
 
 app.listen(port, function() {
     console.log(`My Calculator is listening on port ${port}`);

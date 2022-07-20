f2773834f3301c6589365834712ed4e13f61cfb6
mridle582 <mridle582@gmail.com>
2022-07-20 10:55:39 -0400

    simply calc complete

--- calculator.js
+++ calculator.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const port = 3000;
 
+
 app.get('/', function(req, res){
-    res.send('Hello World');
+    // res.send('Hello World');
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    console.log(req.body);
+    var n1 = Number(req.body.n1);
+    var n2 = Number(req.body.n2);
+    var result = n1 + n2;
+
+    res.send("The result of the calculation is: " + result);
 });
 
 app.listen(port, function() {

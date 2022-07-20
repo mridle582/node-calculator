e0cfddbf9aa513e9ab29d1e1683fb8a2dc9ff76a
mridle582 <mridle582@gmail.com>
2022-07-20 10:20:27 -0400

    calc boiler plate code

--- calculator.js
+++ calculator.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(port, function() {
+    console.log(`My Calculator is listening on port ${port}`);
+});
